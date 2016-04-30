6cc4555afa3e2a9562e005a6ee53d37bb795bced
Jorge Godoy <godoy.jf@gmail.com>
2016-04-30 20:48:31 -0300

    remove unused code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ HandlebarsFilters.prototype.constructor = HandlebarsFilters;
 
 function HandlebarsFilters (tree, options) {
   if (!(this instanceof HandlebarsFilters)) {
-    return new HandlebarsFilters(tree, options)
+    return new HandlebarsFilters(tree, options);
   }
 
   this.options = options || {};
@@ -25,7 +25,7 @@ function HandlebarsFilters (tree, options) {
   this.options.runtimePath = this.options.runtimePath || 'handlebars/dist/handlebars.runtime.js'; 
 
   // Set options necessary for the filter
-  filterOptions = {
+  var filterOptions = {
     srcDir: this.options.srcDir,
     extensions: this.options.extensions,
     targetExtension: this.options.targetExtension
@@ -36,7 +36,6 @@ function HandlebarsFilters (tree, options) {
 
 HandlebarsFilters.prototype.processString = function (string, srcFile) {
   try {
-    var templateName = this.getTemplateName(srcFile);
     var precompiled = this.handlebars.precompile(string, this.options);
     var runtimePath = this.getRuntimePath(srcFile);
     var output = 'var Handlebars = require(\'' + runtimePath + '\'); module.exports = Handlebars.template(' + precompiled + ');';
@@ -45,36 +44,6 @@ HandlebarsFilters.prototype.processString = function (string, srcFile) {
     console.log(err.message);
   }
 };
-
-/**
- * Get the template name
- * @param  {string} srcFile src file path
- * @return {string} template name
- */
-HandlebarsFilters.prototype.getTemplateName = function (srcFile) {
-    var templateName = srcFile;
-
-    //Remove templates path
-    if (this.options.srcDir !== null) {
-      templateName = templateName.replace(this.options.srcDir, '');
-    }
-
-    //Remove / if there is any
-    if (templateName.indexOf('/') === 0) {
-      templateName = templateName.substr(1, templateName.length);
-    }
-
-    //Remove extension
-    for (var i = 0; i < this.options.extensions.length; i++) {
-      var ext = '.' + this.options.extensions[i];
-      var extPos = templateName.indexOf(ext);
-      if (extPos > -1 && extPos === (templateName.length - ext.length)) {
-        templateName = templateName.substr(0, extPos);
-      }
-    }
-
-    return templateName;
-}
 
 /**
  * Get the handlebars runtime path
@@ -90,4 +59,4 @@ HandlebarsFilters.prototype.getRuntimePath = function (srcFile) {
   // path
   var runtimePathAbs = path.resolve(this.options.runtimePath);
   return path.relative(srcFile, runtimePathAbs);
-}
+};
